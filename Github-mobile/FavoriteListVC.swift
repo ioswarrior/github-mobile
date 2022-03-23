1297c48317d2e8353d388b25b651c8d45304d809
iOS Warrior <ioswarrior@gmail.com>
2022-03-23 14:01:35 +0600

    Initial project setup and app navigation setup (UITabBarController & UINavigationController)

--- Github-mobile/FavoriteListVC.swift
+++ Github-mobile/FavoriteListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoriteListVC.swift
 //  Github-mobile
 //
 //  Created by Muller Alexander on 23.03.2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoriteListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBlue
     }
-
-
 }
-
---
+++ Github-mobile/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  Github-mobile
+//
+//  Created by Muller Alexander on 23.03.2022.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
